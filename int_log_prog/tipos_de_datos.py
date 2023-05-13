cf006472d44b77da07d3b1de455ad934785a6b71
aryabhata6 <132781544+aryabhata6@users.noreply.github.com>
2023-05-13 09:59:42 -0600

    Add files via upload

---
+++ int_log_prog/tipos_de_datos.py
@@ -0,0 +1,22 @@
+# Tipos de datos: Clasificacion de los datos
+import datetime
+
+# Declarar las variables
+nombre = 'alexis'
+edad = '27'
+direccion = 'calle x, del. y'
+telefono = '5564897512'
+estatus = True #true/false
+colores_favoritos = ['gris, rojo, azul'] #lista o arreglo
+fecha_nacimiento = datetime.date(96, 2, 26)
+
+# Salida de datos
+print('nombre :', type(nombre))
+print('edad:', type(edad))
+print('direccion: ',type(direccion))
+print('telefono:',type(telefono))
+print('status:',type(estatus))
+print('colores favoritos: ',type(colores_favoritos))
+print('fecha de nacimiento:',type(fecha_nacimiento))
+
+      
